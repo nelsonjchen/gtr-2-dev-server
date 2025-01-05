f215ed47c8b9f120e7b9ee846309616d8ffa8cf3
Nelson Chen <crazysim@gmail.com>
2025-01-05 12:09:38 -0800

    Range header support

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -48,10 +49,56 @@ func main() {
 			return
 		}
 
-		log.Println("Serving test.txt content")
 		w.Header().Set("Content-Type", "text/plain")
 		w.Header().Set("Content-Disposition", "attachment; filename=test.txt")
-		fmt.Fprint(w, testData)
+		w.Header().Set("Accept-Ranges", "bytes")
+
+		fileSize := int64(len(testData))
+		rangeHeader := r.Header.Get("Range")
+
+		if rangeHeader == "" {
+			log.Println("Serving full test.txt content")
+			w.Header().Set("Content-Length", fmt.Sprint(fileSize))
+			fmt.Fprint(w, testData)
+			return
+		}
+
+		// Parse range header
+		rangeParts := strings.Split(rangeHeader, "=")
+		if len(rangeParts) != 2 || rangeParts[0] != "bytes" {
+			http.Error(w, "Invalid Range header", http.StatusBadRequest)
+			return
+		}
+
+		rangeSpec := strings.Split(rangeParts[1], "-")
+		start, err := strconv.ParseInt(rangeSpec[0], 10, 64)
+		if err != nil {
+			http.Error(w, "Invalid Range header", http.StatusBadRequest)
+			return
+		}
+
+		var end int64
+		if rangeSpec[1] == "" {
+			end = fileSize - 1
+		} else {
+			end, err = strconv.ParseInt(rangeSpec[1], 10, 64)
+			if err != nil || end >= fileSize {
+				end = fileSize - 1
+			}
+		}
+
+		if start > end || start < 0 {
+			http.Error(w, "Invalid Range header", http.StatusBadRequest)
+			return
+		}
+
+		contentLength := end - start + 1
+		w.Header().Set("Content-Length", fmt.Sprint(contentLength))
+		w.Header().Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", start, end, fileSize))
+		w.WriteHeader(http.StatusPartialContent)
+
+		log.Printf("Serving partial content: bytes %d-%d/%d", start, end, fileSize)
+		fmt.Fprint(w, testData[start:end+1])
 	})
 
 	log.Println("Starting server on :8080")
